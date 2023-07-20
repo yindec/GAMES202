58fce59eff8c50ff92b000361abaa0de94311529
yindec <yindec17@outlook.com>
2023-07-20 12:55:04 +0800

    初始化

---
+++ homework1/homework1/src/lights/DirectionalLight.js
@@ -0,0 +1,53 @@
+class DirectionalLight {
+
+    constructor(lightIntensity, lightColor, lightPos, focalPoint, lightUp, hasShadowMap, gl) {
+        this.mesh = Mesh.cube(setTransform(0, 0, 0, 0.2, 0.2, 0.2, 0));
+        this.mat = new EmissiveMaterial(lightIntensity, lightColor);
+        this.lightPos = lightPos;
+        this.focalPoint = focalPoint;
+        this.lightUp = lightUp
+
+        this.hasShadowMap = hasShadowMap;
+        this.fbo = new FBO(gl);
+        if (!this.fbo) {
+            console.log("无法设置帧缓冲区对象");
+            return;
+        }
+    }
+
+    CalcLightMVP(translate, scale) {  
+        let lightMVP = mat4.create();  
+        let modelMatrix = mat4.create();  
+        let viewMatrix = mat4.create();  
+        let projectionMatrix = mat4.create();  
+    
+        //https://glmatrix.net/docs/module-mat4.html
+    
+        //Edit Start  
+    
+        // Model transform  
+        mat4.translate(modelMatrix, modelMatrix, translate)  
+        mat4.scale(modelMatrix, modelMatrix, scale)  
+    
+        // View transform  
+        mat4.lookAt(viewMatrix, this.lightPos, this.focalPoint, this.lightUp)  
+    
+        // Projection transform  
+        var r = 100;  
+        var l = -r;  
+        var t = 100;  
+        var b = -t;  
+    
+        var n = 0.01;  
+        var f = 400;  // f 值不合适的话，会很离谱，可以多尝试几次。
+    
+        mat4.ortho(projectionMatrix, l, r, b, t, n, f);  
+    
+        //Edit End  
+    
+        mat4.multiply(lightMVP, projectionMatrix, viewMatrix);  
+        mat4.multiply(lightMVP, lightMVP, modelMatrix);  
+    
+        return lightMVP;  
+    }
+}
