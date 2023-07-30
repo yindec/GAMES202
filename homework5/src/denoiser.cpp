e83b8df112755e69694b0a4c64c7e45b277cc649
yindec <yindec17@outlook.com>
2023-07-30 13:11:49 +0800

    单帧降噪

--- homework5/src/denoiser.cpp
+++ homework5/src/denoiser.cpp
@@ -41,12 +41,55 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
     int height = frameInfo.m_beauty.m_height;
     int width = frameInfo.m_beauty.m_width;
     Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
-    int kernelRadius = 16;
+    int kernelRadius = 32;      // 64^2
+
+//是OpenMP中的一个指令，表示接下来的for循环将被多线程执行，另外每次循环之间不能有关系 collapse(2) 两重循环
 #pragma omp parallel for
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             // TODO: Joint bilateral filter
-            filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            //filteredImage(x, y) = frameInfo.m_beauty(x, y);
+
+            int x_start = std::max(x - kernelRadius, 0);
+            int x_end   = std::min(x + kernelRadius, width - 1);
+            int y_start = std::max(y - kernelRadius, 0);
+            int y_end   = std::min(y + kernelRadius, height - 1);
+
+            auto center_position = frameInfo.m_position(x, y);
+            auto center_normal   = frameInfo.m_normal(x, y);
+            auto center_color    = frameInfo.m_beauty(x, y);
+
+            Float3 final_color;
+            auto total_weight =  0.0f;
+
+            for (int m = x_start; m <= x_end; m++) {
+                for (int n = y_start; n <= y_end; n++) {
+
+                    auto postion = frameInfo.m_position(m, n);
+                    auto normal  = frameInfo.m_normal(m, n);
+                    auto color   = frameInfo.m_beauty(m, n);
+
+                    auto d_position = SqrDistance(center_position, postion) / (2.0f * m_sigmaCoord);
+                    auto d_color    = SqrDistance(center_color, color) / (2.0f * m_sigmaColor);
+                    auto d_normal   = SafeAcos(Dot(center_normal, normal));
+                    d_normal *= d_normal;
+                    d_normal /= (2.0f * m_sigmaNormal);
+
+                    float d_plane = .0f;
+                    if (d_position > 0.f) { // 防止 Normalize(postion - center_position) 出现问题
+                        d_plane = Dot(center_normal, Normalize(postion - center_position));
+                    }
+                    d_plane *= d_plane;
+                    d_plane /= (2.0f * m_sigmaPlane);
+
+                    // d_xx > 0, 各个系数 d_xx 越小，(-d_plane - d_position - d_color - d_normal)越趋近于 -0， weight越趋近于1，也就越大, 符合物理意义
+                    float weight = std::exp(-d_plane - d_position - d_color - d_normal);        
+                    total_weight += weight;
+                    final_color += color * weight;
+                }
+            }
+
+            filteredImage(x, y) = final_color / total_weight;
         }
     }
     return filteredImage;
